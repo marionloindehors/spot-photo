0d54e10bf4a622fbbf21d9d52878b00f75d8fa19
Thibault de Roquefeuil <thibault.roquefeuil@gmail.com>
2022-12-02 12:12:20 +0100

    add transformer to requirement,  et un début de fonction load_captionning_model()

--- spot-photo/ml_logic/model.py
+++ spot-photo/ml_logic/model.py
@@ -1,4 +1,18 @@
+from PIL import Image
+from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
+import torch
+
+
+
+
+
 # DEF A FUNCTION TO LOAD CAPTIONING MODEL
+
+def load_captionning_model():
+    model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
 
 # DEF A FUNCTION TO LOAD SENTENCE SIMILARITY MODEL
 
