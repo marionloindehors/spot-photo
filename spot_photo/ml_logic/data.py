89772b4b0ce491265d07fc1709cf6c8342e50683
Marion Farout <mfarout@gmail.com>
2022-12-05 12:03:33 +0100

    start interface, modif model

--- spot_photo/ml_logic/data.py
+++ spot_photo/ml_logic/data.py
@@ -39,9 +39,6 @@ def encode_X_pred(model, corpus_X_pred):
 def load_data(bucket_name = 'bucket_image_flickr30k',
                 file_name = 'flickr30k_captions.csv'):
 
-
-
-
     credentials = service_account.Credentials.from_service_account_file(
     'possible-aspect-369317-239ac2560a56.json')
 
@@ -61,7 +58,6 @@ def load_data(bucket_name = 'bucket_image_flickr30k',
     #url = f'gs://{bucket_name}/{file_name}'
 
     data = pd.read_csv(file_name, on_bad_lines='skip', delimiter='|')
-    print(data['image_name'][55])
     return data
 
 
--- spot_photo/streamlit/app.py
+++ spot_photo/streamlit/app.py
@@ -2,14 +2,21 @@
 import streamlit as st
 import requests
 
+st.write("""Welcome on spot photo !""")
+
 with st.form():
-    query = st.text_input('Which photo are you looking for ?', 'Please enter your description')
+    query = st.text_input('Which photo are you looking for ?', 'Please enter a description')
+
+    model_choice = st.multiselect('Choose your model', ['all-mpnet-base-v2','clip-ViT-B-32' ])
 
     st.form_submit_button('Search')
 
-params = dict(query= query)
+results = st.image('./header.png')
 
-spot_photo_api_url = 'https://taxifare.lewagon.ai/predict'
+
+params = dict(query=query)
+
+spot_photo_api_url = 'https://taxifare.lewagon.ai/predict' #A CHANGER
 response = requests.get(spot_photo_api_url, params=params)
 
 prediction = response.json()
