b05fb814412d8b9d289d99f8d6ce0abe6a56328b
Marion Farout <mfarout@gmail.com>
2022-12-05 16:06:10 +0100

    fast and api OK! streamlit add to requirements

--- spot_photo/streamlit/app.py
+++ spot_photo/streamlit/app.py
@@ -6,40 +6,43 @@ from google.cloud import storage
 from PIL import Image
 from io import BytesIO
 
+st.title(" Welcome to Spot Photo 🔍 ")
+st.subheader("A wonderful app to look after your f**cking holidays pictures during dark and rainy days")
+
 
 query = st.text_input('Which photo are you looking for ?', 'Please enter your description')
 
-#st.form_submit_button('Search')
+
+k = st.slider('Number of pictures to display', 1, 5, 3)
+
+if query is not None:
+    params = dict(query=query, k=k)
 
 
+    spot_photo_api_url = 'http://127.0.0.1:8000/recherche'
+    response = requests.get(spot_photo_api_url, params=params)
 
+    if response.ok:
+        #st.markdown(f'{response.json()}')
 
-k = st.slider('number of pictures display', 1, 5, 3)
-params = dict(query=query, k=k)
+        credentials = service_account.Credentials.from_service_account_file('possible-aspect-369317-b19475afaf02.json')
 
+        client = storage.Client(credentials=credentials)
+        #client = storage.Client('possible-aspect-369317')
+        bucket_name = 'bucket_image_flickr30k'
 
-spot_photo_api_url = 'http://127.0.0.1:8000/recherche'
-response = requests.get(spot_photo_api_url, params=params).json()
-#st.markdown(f'{response.keys()}')
+        bucket = client.get_bucket(bucket_name)
 
-
-credentials = service_account.Credentials.from_service_account_file(
-'possible-aspect-369317-b19475afaf02.json')
-
-client = storage.Client(credentials=credentials)
-#client = storage.Client('possible-aspect-369317')
-bucket_name = 'bucket_image_flickr30k'
-
-bucket = client.get_bucket(bucket_name)
-
-
-blob_l =[]
-for image in response :
-    file_name = f"flickr30k_images/{image}"
-    blob = bucket.get_blob(file_name)
-    blob_l.append(blob)
-rows = len(response)
-for  x in range(rows):
-    blob_n = blob_l[x]
-    img = Image.open(BytesIO(blob_n.download_as_bytes()))
-    st.image(img)
+        blob_l =[]
+        d = response.json()
+        for image in d.keys() :
+            file_name = f"flickr30k_images/{image}"
+            blob = bucket.get_blob(file_name)
+            blob_l.append(blob)
+        rows = len(d.keys())
+        for x in range(rows):
+            blob_n = blob_l[x]
+            img = Image.open(BytesIO(blob_n.download_as_bytes()))
+            st.image(img)
+    else:
+        st.write(response.json())
