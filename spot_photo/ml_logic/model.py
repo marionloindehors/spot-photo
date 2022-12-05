0817d514e6d227b211b0399fe8a8a839c3bc00a5
Thibault de Roquefeuil <thibault.roquefeuil@gmail.com>
2022-12-05 18:30:22 +0100

    add a function to caption new images from bucket

--- spot_photo/ml_logic/model.py
+++ spot_photo/ml_logic/model.py
@@ -9,39 +9,43 @@ from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTok
 
 
 
-
+#---------------------------------------
 # DEF A FUNCTION TO LOAD CAPTIONING MODEL
-
-# def load_captionning_model():
-#     model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-#     feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-#     tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-
-#     max_length = 20 #Nombre de mots dans la caption
-#     num_beams = 4 #on ne sait pas ce qu'on sait
-#     gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
-#     def predict_step(image_paths):
-#         images = []
-#         for image_path in image_paths:
-#             i_image = Image.open(BytesIO(image_path.download_as_bytes()))
-#             if i_image.mode != "RGB":
-#                 i_image = i_image.convert(mode="RGB")
-
-#             images.append(i_image)
-
-#         pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
-#         #pixel_values = pixel_values.to(device)
-
-#         output_ids = model.generate(pixel_values, **gen_kwargs) #matrice des captions encodées (?)
-
-#         preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True) #decode les matrices en mots
-#         preds = [(image.name, pred.strip()) for image, pred in zip(image_paths,preds)] #list de tuple ( nom image,  caption)
-#         #preds = [ pred.strip() for pred in preds]  #list de chaque caption
-#         return preds
-#      return X_pred #CSV WITH CAPTIONING
+#---------------------------------------
+def load_captionning_model():
+    model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+    return model, feature_extractor, tokenizer
 
 
+def predict_step(model, feature_extractor, tokenizer, list_of_blob):
+    max_length = 20 #Nombre de mots dans la caption
+    num_beams = 4 #on ne sait pas ce qu'on sait
+    gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+    images = []
+    for blob in list_of_blob:
+        i_image = Image.open(BytesIO(blob.download_as_bytes()))
+        if i_image.mode != "RGB":
+            i_image = i_image.convert(mode="RGB")
+
+        images.append(i_image)
+    print(len(images))
+    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+    #pixel_values = pixel_values.to(device)
+
+    output_ids = model.generate(pixel_values, **gen_kwargs) #matrice des captions encodées (?)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True) #decode les matrices en mots
+    preds = [(image.name, pred.strip()) for image, pred in zip(list_of_blob,preds)] #list de tuple ( nom image,  caption)
+    #preds = [ pred.strip() for pred in preds]  #list de chaque caption
+    return preds  #list de tuple ( nom image,  caption)
+
+
+#------------------------------------------------
 # DEF A FUNCTION TO LOAD SENTENCE SIMILARITY MODEL
+#-------------------------------------------------
 def load_sentence_similarity_model(model_name='all-mpnet-base-v2'):
     # Instanciate Model
     if model_name == 'all-mpnet-base-v2':
@@ -50,12 +54,17 @@ def load_sentence_similarity_model(model_name='all-mpnet-base-v2'):
         model = SentenceTransformer('clip-ViT-B-32')
     return model
 
+#--------------------------------
+# DEF A FUNCTION TO EMBEDD A QUERY
+#--------------------------------
 def embedding_query(model, query):
     # Embedding query
     query_embedding = model.encode(query) #/!\ clip vit return matrice shape 512!!
     return query_embedding
 
-
+#---------------------------------------
+# DE A FUNCTION TO COMPUTE THE SIMILARITY
+#-----------------------------------------
 def compute_similarity(query_embedding, X_pred_embeddings, k=2): #images_embedding
 
     hits = util.semantic_search(query_embedding, X_pred_embeddings, top_k=k)
