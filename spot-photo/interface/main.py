573738fca417c4268f2c63d5ab545d3b423d7d76
Guillaume Alleon <guillaume.alleon@gmail.com>
2022-12-06 08:41:20 +0100

    Deleting spot-photo

--- spot-photo/interface/main.py
+++
@@ -1,5 +0,0 @@
-from ml_logic.data import load_X_pred
-
-X_pred = load_X_pred(bucket_name = 'bucket_image_flickr30k',
-                file_name = 'X_pred_caption_0_to_1000.csv')
-print(X_pred.shape)
